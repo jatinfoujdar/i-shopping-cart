4baa0a125273c48e4ddd6305945f9571bd609233
jatin foujdar <jatin321001@gmail.com>
2022-12-21 23:56:54 +0530

    changes commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <Link to="/">Shopping Cart</Link>
+          <Link to="/">i-Shopping-Cart</Link>
         </Navbar.Brand>
         {useLocation().pathname.split("/")[1] !== "cart" && (
           <Navbar.Text className="search">
