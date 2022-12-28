040c6d8640f2f85de92eae8a7de6e7f8dc3bb540
jatin foujdar <53086199+jatinfoujdar@users.noreply.github.com>
2022-12-28 16:32:50 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
   } = CartState();
 
   return (
-    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+    <Navbar bg="purple" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
           <Link to="/">i-Shopping-Cart</Link>
